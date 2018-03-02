e66bdbb9842beeba2263d744bdbe1bc33270755f
Yizhi Yin <yizhiyin86@gmail.com>
2018-03-01 21:33:41 -0600

    Ok, add some code

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,11 +4,10 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
-
+consumer_key = "6dj1rPeIXm9lGHAJ0XDOlBCKT"
+consumer_secret = "Czsnhwgan8wDkShkWclER9XNcpurhJFWmCBaC5EvVv5BcKPhHV"
+access_token = "968998318780174336-zgI9bmi7Oaf3FbpeaIP6SxpFN85LewW"
+access_token_secret ="kttUSYIn0NmynHBswVRehSC0BBV2akgqk4hfQ6i5VlFK7"
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -17,3 +16,6 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Create a function that tweets
 # CODE GOES HERE
+while True:
+    api.update_status('hello')
+    time.sleep(30)
